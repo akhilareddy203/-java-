3ea0b0746fd6cd5f2e6dfa52f647b22b2327df68
akhilareddy203 <152789725+akhilareddy203@users.noreply.github.com>
2024-07-01 11:22:47 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+class String
+{
+	public static void main(String[] args)
+	{
+		String s1=new String("Java");
+		System.out.println(s1);
+		s1.concat("language");
+		System.out.println(s1);
+}
+}
