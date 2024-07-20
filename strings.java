56706de69cef2b76355affab9631f871be325f83
akhilareddy203 <152789725+akhilareddy203@users.noreply.github.com>
2024-07-20 08:22:13 +0530

    Create strings.java

---
+++ strings.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.String;
+class Testex
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+int b=s.nextInt();
+int c=a/b;
+System.out.println(c);
+System.out.println("End of program");
+}
+}
